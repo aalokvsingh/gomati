d3b78e95a0f808717c21ed32f01b7aafba5b43f8
aalokvsingh <aalokvsingh@gmail.com>
2019-08-08 14:01:47 +0530

    about page

--- views/site/about.php
+++ views/site/about.php
@@ -10,9 +10,51 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        This is the About page. You may modify the following file to customize its content:
+    <!-- <p>
+    We are leading Manufacturer and trader of a wide range of Precast molds and its equipment.We have highly skilled professionals and large fabrication unit to providing high quality product to our clients. Some of our offered products are Precast Wall Mould, Precast Manhole Mold, Fencing Pole Line Mold, Precast Drain Mold, Concrete Mold, etc.
     </p>
-
-    <code><?= __FILE__ ?></code>
+    <p>Precast Molds are manufactured using high grade quality components that are sourced from certified vendors of the market in compliance with international quality standards. We provide installation services to our customers.</p>
+    <p>Our Molds are highly appreciated by our customer for their significance such as robust construction buinding, minimal maintenance cost, high performance and longer service life.</p>
+    <p>We have established a huge client base across the nation due to the transparency in our business dealings.As mentioned bellow</p>
+    <ul>
+    <li>Shapoorji Pallonji & Co. Pvt. Ltd.</li>
+    <li>L&T Construction</li>
+    <li>M. Pallonji & Co. Pvt. Ltd.</li>
+    <li>Hindustan Platinum Pvt. Ltd</li>
+    </ul> -->
+    <p>We take pride to introduce ourselves as trusted and leading Manufacturer and trader of a wide range of Precast molds and its equipment.Our molds are Manufactured using optimum quality material that are sourced from certified vendors in the market in compliance with international quality standards, these products are acknowledged for dimensional accuracy, durability, corrosion resistance, robustness and high tensile strength. We leave no stone unturned in offering excellent quality products and services to the clients.</p>
+    <p>To keep pace with the ever increasing demands of the clients, We have facilated with advanced machines and technology, with our in-house units for M.S. & SS Fabrication, P.M.C for Structural calculation, auto-cad design facilities and other amenities, we have been able to design and develop products as per the specific requirements of the clients. All these factors along with time lined delivery of the consignments have helped us in garnering a huge client base in country. Our professionals also help us in maintaining cordial and long term relations with the valued clients including Multiplex, Phoenix Industries, Bharat Enterprises, Coca Cola, Hindustan Platinum Pvt. Ltd., Ameya and Symbiosis International University.</p>
+    <p>Our Products Range</p>
+    <ul>
+    <li>RCC Precast Work</li>
+    <li>Precast moulds fabrication & Design</li>
+    <li>Fabricating All type of Moulds</li>
+    <li>Staircase Mould</li>
+    <li>Flat Slab Mould with Tilting Manual & hydraulic system</li>
+    <li>C-Type & L-Type Wall panel Moulds</li>
+    <li>Beam Moulds</li>
+    </ul>
+    <h3>Why US?</h3>
+    We have gained competitive edge over our competitors owing to the following aspects:
+    <ul>
+    <li>Quality products</li>
+    <li>Timely delivery</li>
+    <li>Customized solution</li>
+    <li>Ethical business practices</li>
+    </ul>
+    <h3>Clients / Customers</h3>
+    <ul>
+    <li>Shapoorji Pallonji & Co. Pvt. Ltd.</li>
+    <li>L&T Construction</li>
+    <li>Valmark Developers Pvt. Ltd.</li>
+    <li>Ameya Buildcons Pvt. Ltd.</li>
+    <li>Vascon Engineers Ltd.</li>
+    <li>M. Pallonji & Co. Pvt. Ltd.</li>
+    <li>Pratham</li>
+    <li>C-SUM Engineers</li>
+    <li>Bharat Engineering Works</li>
+    <li>Totem Infrastructure Ltd.</li>
+    <li>MBM Precast India Pvt. Ltd.</li>
+    <li>Mahindra & Mahindra</li>
+    </ul>
 </div>
